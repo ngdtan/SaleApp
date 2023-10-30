e0f160e3e68e5cf10eab4a509eba3077ded5092a
ngdtan <ngdtan852003@gmail.com>
2023-10-30 11:35:45 +0700

    init project

---
+++ saleapp/dao.py
@@ -0,0 +1,52 @@
+def load_categories():
+    return [{
+        'id': 1,
+        'name': 'Mobile'
+    }, {
+        'id': 2,
+        'name': 'Tablet'
+    }]
+
+
+def load_products(kw=None):
+    products = [{
+        'id': 1,
+        'name': 'iPhone 13',
+        'price': 20000000,
+        'image': 'https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg'
+    }, {
+        'id': 2,
+        'name': 'Galaxy S22',
+        'price': 20000000,
+        'image': 'https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg'
+    }, {
+        'id': 1,
+        'name': 'iPhone 13',
+        'price': 20000000,
+        'image': 'https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg'
+    }, {
+        'id': 1,
+        'name': 'iPhone 13',
+        'price': 20000000,
+        'image': 'https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg'
+    }, {
+        'id': 1,
+        'name': 'iPhone 13',
+        'price': 20000000,
+        'image': 'https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg'
+    }, {
+        'id': 1,
+        'name': 'iPhone 13',
+        'price': 20000000,
+        'image': 'https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg'
+    }, {
+        'id': 1,
+        'name': 'iPhone 13',
+        'price': 20000000,
+        'image': 'https://res.cloudinary.com/dxxwcby8l/image/upload/v1688179242/hclq65mc6so7vdrbp7hz.jpg'
+    }]
+
+    if kw:
+        products = [p for p in products if p['name'].find(kw) >= 0]
+
+    return products
